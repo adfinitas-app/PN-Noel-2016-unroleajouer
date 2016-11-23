a0c92821d96c7c3d72e2c623c0ce344a94b29474
bruere_s <sebastien.bruere@epitech.eu>
2016-11-23 15:37:22 +0100

    woopra

---
+++ assets/javascript/woopra.js
@@ -0,0 +1,15 @@
+$('#seeIntro').click(function(){
+	woopra.track('interaction', {category:"SPOT_LP_unroleajouer16",action:"clic",url:document.location.href,title: document.title});
+});
+$('#seeMinute').click(function(){
+	woopra.track('interaction', {category:"VIDEO1MIN_LP_unroleajouer16",action:"clic",url:document.location.href,title: document.title});
+});
+$('#share-fb').click(function(){
+	woopra.track('interaction', {category:"PARTAGEFB_LP_unroleajouer16",action:"clic",url:document.location.href,title: document.title});
+});
+$('#share-tw').click(function(){
+	woopra.track('interaction', {category:"PARTAGETW_LP_unroleajouer16",action:"clic",url:document.location.href,title: document.title});
+});
+$('#dw-kit').click(function(){
+	woopra.track('interaction', {category:"KITAMBASSADEUR_LP_unroleajouer16",action:"clic",url:document.location.href,title: document.title});
+});
